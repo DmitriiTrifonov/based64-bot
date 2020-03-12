fef4e29dc7dcb7786a027cf3f8b0a563930c9930
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2020-03-12 11:48:08 +0300

    [file] -- base64 goes to file

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -32,6 +35,8 @@ func main() {
 		"It converts Images to Base64 strings and vice versa\n" +
 		"Send a photo to start"
 
+	errorMessage := "Cannot process the Image"
+
 	b.Handle("/start", func(m *tb.Message) {
 		_, _ = b.Send(m.Sender, message)
 	})
@@ -48,23 +53,32 @@ func main() {
 		photoUrl := m.Photo.File.FileID
 		url, err := b.FileURLByID(photoUrl)
 		if err != nil {
-			_, _ = b.Send(m.Sender, "Cannot process the photo")
+			_, _ = b.Send(m.Sender, errorMessage)
 			return
 		}
 		log.Println(url)
 		resp, err := http.Get(url)
 		if err != nil {
-			_, _ = b.Send(m.Sender, "Cannot process the photo")
+			_, _ = b.Send(m.Sender, errorMessage)
 			return
 		}
 		bytes, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			_, _ = b.Send(m.Sender, "Cannot process the photo")
+			_, _ = b.Send(m.Sender, errorMessage)
 			return
 		}
 		enc := base64.StdEncoding.EncodeToString(bytes)
 		log.Println(enc)
-		_, _ = b.Send(m.Sender, enc)
+		rand.Seed(time.Now().UnixNano())
+		n := rand.Intn(1000)
+		s := strconv.Itoa(n)
+		file, err := os.Create(s + ".txt")
+		if file != nil {
+			_, err = file.Write([]byte(enc))
+			err = file.Close()
+		}
+		p := &tb.Document{File: tb.FromDisk(s + ".txt"), FileName: "base64.txt"}
+		_, _ = b.Send(m.Sender, p)
 	})
 
 	b.Start()
